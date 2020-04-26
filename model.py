003886b0ba283078b76d24db9b7cb7be8a9f7b5e
ashutoshpatil603 <55447268+ashutoshpatil603@users.noreply.github.com>
2020-04-26 10:24:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -16,9 +16,9 @@ df = pd.concat([df, dummy_sex, dummy_region, dummy_smoker, dummy_region], axis=1
 df.drop(['region', 'southwest', 'sex', 'smoker'], axis=1, inplace=True)
 
 # now lets analyse
-# plt.scatter(df['age'], df['charges'])
-# plt.scatter(df['smoker'],df['charges'])
-# plt.show()
+plt.scatter(df['age'], df['charges'])
+plt.scatter(df['smoker'],df['charges'])
+plt.show()
 # here we get that age is made impact on our regression
 # analysis contains that each x is how affect y or how it is related to y
 
@@ -55,8 +55,8 @@ print(model.score(x_test, y_test))
 
 model_ridge = Ridge(alpha=0.5)
 model_ridge.fit(x_train, y_train)
-# print(model_ridge.score(x_test, y_test))
-# print(model_ridge.score(x_train, y_train))
+print(model_ridge.score(x_test, y_test))
+print(model_ridge.score(x_train, y_train))
 
 # lets calculate r_squared
 y_pred = model.predict(x_test)
